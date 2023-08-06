264f23a666f07e3372569c16aacdf23b07922a15
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-07 00:25:50 +0500

    Scaffold!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.green.shade300,
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade600,
+        centerTitle: true,
+        title: const Text("LearnFlutter"),
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              "https://assets.skyfilabs.com/images/blog/best-online-courses-to-learn-flutter-for-beginners.webp"),
+        ),
+      ),
+    ),
+  ));
 }
