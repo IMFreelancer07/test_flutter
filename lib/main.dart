0ae7a309332e4bb70550179e18d81ecb72b933e9
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-07 23:32:46 +0500

    SingleChildWidgets!

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.green.shade300,
-      appBar: AppBar(
-        backgroundColor: Colors.green.shade600,
-        centerTitle: true,
-        title: const Text("LearnFlutter"),
-      ),
-      body: const Center(
-        child: Image(
-          image: AssetImage("assets/test.jpg"),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green.shade300,
+        appBar: AppBar(
+          backgroundColor: Colors.green.shade600,
+          centerTitle: true,
+          title: const Text("Learn Flutter"),
+        ),
+        body: Center(
+          child: Container(
+            height: 250,
+            width: 250,
+            color: Colors.grey[700],
+            child: Center(
+              child: Text("Container"),
+            ),
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
