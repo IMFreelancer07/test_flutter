aae9c4c0b56ff06f473f8ddf6b298ae7b57ad363
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-11 23:18:30 +0500

    stful lottery App!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -12,28 +13,65 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int x = 10;
+
+  Random random = Random();
+  int x = 0;
+  int y = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-            child: Center(
-                child: Text(
-                  "$x",
-                  style: TextStyle(fontSize: 50),
-                )
-            )
+        appBar: AppBar(
+          title: Text("LotteryApp"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(child: Text("Lottery Winning Number is $x")),
+            SizedBox(height: 20,),
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.withOpacity(.3),
+                borderRadius: BorderRadius.circular(15)
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child:
+                x == y && x !=0?
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.done_all_outlined, color: Colors.green, size: 30,),
+                    SizedBox(height: 15,),
+                    Text("Hurrah! Your number is $y. \n You've won the lottery!!!", textAlign: TextAlign.center,),
+                  ],
+                ) :
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.error, color: Colors.red, size: 30,),
+                    SizedBox(height: 15,),
+                    Text("Better luck next time! Your number is $y. \n Try again!", textAlign: TextAlign.center,),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (){
-            x++;
+            x = random.nextInt(10);
+            y = random.nextInt(10);
             setState(() {
 
             });
           },
-          child: Icon(Icons.add_circle),
+          child: Icon(Icons.refresh),
         ),
       ),
     );
