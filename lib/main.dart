0d77a6423974a5cfe29d1585b374a84ee9cd2bb8
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-12 23:56:36 +0500

    wip!

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Center(child: Text("Learning Widgets")),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                height: 200,
-                width: 250,
-                padding: EdgeInsets.all(0),
-                transform: Matrix4.rotationZ(.15),
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: .5
-                  ),
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage("https://blog.logrocket.com/wp-content/uploads/2021/05/intro-dart-flutter-feature.png"),
-                  ),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.deepPurpleAccent,
-                      blurRadius: 10,
-                    )
-                  ],
-                ),
-                child: Center(child: Text("Flutter Image Button")),
-              ),
-            )
-          ],
-        ),
+        body: Center(child: Text("Work in progress...!")),
       ),
     );
   }
