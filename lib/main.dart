2274a136ff8937fdf1bd2137560c65a9326fb550
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-12 23:55:29 +0500

    Container Widget!

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Learning Widgets"),
+          title: Center(child: Text("Learning Widgets")),
         ),
-        body: Center(child: Text("Work in progress...!")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                height: 200,
+                width: 250,
+                padding: EdgeInsets.all(0),
+                transform: Matrix4.rotationZ(.15),
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: .5
+                  ),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage("https://blog.logrocket.com/wp-content/uploads/2021/05/intro-dart-flutter-feature.png"),
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.deepPurpleAccent,
+                      blurRadius: 10,
+                    )
+                  ],
+                ),
+                child: Center(child: Text("Flutter Image Button")),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
-
