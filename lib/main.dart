5cece94daf975cad5e09ea06b9d347f8861a2fba
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-12 23:20:46 +0500

    wip!

--- lib/main.dart
+++ lib/main.dart
@@ -5,77 +5,20 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  Random random = Random();
-  int x = 0;
-  int y = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("LotteryApp"),
+          title: Text("Learning Widgets"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(child: Text("Lottery Winning Number is $x")),
-            SizedBox(height: 20,),
-            Container(
-              height: 250,
-              width: 300,
-              decoration: BoxDecoration(
-                color: x==y ? Colors.teal : Colors.blueGrey.withOpacity(.3),
-                borderRadius: BorderRadius.circular(15)
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child:
-                x == y && x !=0?
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.done_all_outlined, color: Colors.green, size: 30,),
-                    SizedBox(height: 15,),
-                    Text("Hurrah! Your number is $y. \n You've won the lottery!!!", textAlign: TextAlign.center,),
-                  ],
-                ) :
-
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.error, color: Colors.red, size: 30,),
-                    SizedBox(height: 15,),
-                    Text("Better luck next time! Your number is $y. \n Try again!", textAlign: TextAlign.center,),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            x = random.nextInt(10);
-            y = random.nextInt(10);
-            setState(() {
-
-            });
-          },
-          child: Icon(Icons.refresh),
-        ),
+        body: Center(child: Text("Work in progress...!")),
       ),
     );
   }
 }
+
 
