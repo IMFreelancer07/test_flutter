cc41ae4336593ee1aaa7d2907b3e49dec5aa8f92
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-13 00:09:34 +0500

    wip!

--- lib/main.dart
+++ lib/main.dart
@@ -15,34 +15,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Center(child: Text("Learning Widgets")),
         ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.white,
-                    height: 250,
-                    child: Center(
-                      child: Text("Jeeay"),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    color: Colors.green,
-                    height: 250,
-                    child: Center(
-                      child: Text("Pakistan"),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+        body: Center(child: Text("Work in progress...!")),
       ),
     );
   }
