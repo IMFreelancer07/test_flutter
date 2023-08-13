5372ac82d32b325d19a826d4bbe2e81ccafd3aca
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-14 00:43:52 +0500

    wip!

--- lib/main.dart
+++ lib/main.dart
@@ -17,27 +17,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Center(child: Text("Learning Widgets")),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: 50,
-                  itemBuilder: (context, index){
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage("https://cdn1.iconfinder.com/data/icons/bokbokstars-121-classic-stock-icons-1/512/person-man.png"),
-                      ),
-                      title: Text("Syed"),
-                      subtitle: Text("Computer Scientist..."),
-                      trailing: Text("$x"),
-                    );
-                  }
-              ),
-            ),
-          ],
-        ),
+        body: Center(child: Text("Work in progress...!")),
       ),
     );
   }
