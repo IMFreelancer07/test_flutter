ea2aa380b38d058b20b52d51da0fb86bf88fe811
IMFreelancer07 <imfreelancer07@gmail.com>
2023-09-03 23:38:36 +0500

    wip!

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:test_flutter/home_screen.dart';
-import 'package:test_flutter/multirole_App/multi_role_home.dart';
-import 'package:test_flutter/multirole_App/splash_screen.dart';
-import 'package:test_flutter/navigations/nav_screen_two.dart';
-import 'package:test_flutter/navigations/nav_third_screen.dart';
-import 'package:test_flutter/utils/routes.dart';
-import 'package:test_flutter/utils/routes_name.dart';
-
-import 'navigations/nav_home.dart';
 
 void main() {
   runApp(MyApp());
