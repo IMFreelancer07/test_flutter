650ea9e4be847fd6b896abb9029733db026455fc
IMFreelancer07 <imfreelancer07@gmail.com>
2023-09-13 00:24:35 +0500

    covidApp trackCountryCases!

--- lib/covid_tracker_app/Services/covid_states_service.dart
+++ lib/covid_tracker_app/Services/covid_states_service.dart
@@ -19,4 +19,18 @@ class CovidStateServices {
     }
 
   }
+
+  Future<List<dynamic>> fetchCountriesRecords () async {
+    final response = await http.get(Uri.parse(covid_AppUrls.countriesList));
+    var data;
+
+    if(response.statusCode == 200 ) {
+      data = jsonDecode(response.body);
+
+      return data;
+    }else{
+      throw Exception("Error fetching data!");
+    }
+
+  }
 }
--- lib/covid_tracker_app/view/covidCountries_list.dart
+++ lib/covid_tracker_app/view/covidCountries_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:test_flutter/covid_tracker_app/Services/covid_states_service.dart';
 
 class CovidCountriesList extends StatefulWidget {
   const CovidCountriesList({super.key});
@@ -8,12 +10,109 @@ class CovidCountriesList extends StatefulWidget {
 }
 
 class _CovidCountriesListState extends State<CovidCountriesList> {
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
 
-      ],
+    CovidStateServices statesServices = CovidStateServices();
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: searchController,
+                onChanged: (value){
+                  setState(() {
+
+                  });
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  hintText: "Search country name...",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                child: FutureBuilder(
+                    future: statesServices.fetchCountriesRecords(),
+                    builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
+                      if(!snapshot.hasData){
+                        return ListView.builder(
+                            itemCount: 5,
+                            itemBuilder: (context, index){
+                              return Shimmer.fromColors(
+                                  baseColor: Colors.grey.shade700,
+                                  highlightColor: Colors.grey.shade100,
+                                  child: Column(
+                                    children: [
+                                      ListTile(
+                                        title: Container(height: 10, width: 80, color: Colors.black54,),
+                                        subtitle: Container(height: 10, width: 80, color: Colors.black54,),
+                                        leading: Container(height: 50, width: 50, color: Colors.black54,),
+                                      ),
+                                    ],
+                                  ),
+                              );
+                            });
+                      }else{
+                        return ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index){
+                              String countryName = snapshot.data![index]['country'];
+
+                              if(searchController.text.isEmpty){
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      title: snapshot.data![index]['country'],
+                                      subtitle: snapshot.data![index]['cases'],
+                                      leading: Image(
+                                        height: 50,
+                                        width: 50,
+                                        image: NetworkImage(
+                                            snapshot.data![index]['countryInfo']['flag']
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }else if(countryName.toLowerCase().contains(searchController.text.toLowerCase())){
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      title: snapshot.data![index]['country'],
+                                      subtitle: snapshot.data![index]['cases'],
+                                      leading: Image(
+                                        height: 50,
+                                        width: 50,
+                                        image: NetworkImage(
+                                            snapshot.data![index]['countryInfo']['flag']
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              } else {
+                                return Container();
+                              }
+                        });
+                      }
+                }),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
