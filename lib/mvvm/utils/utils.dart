32c58ec159828b398e008617f2d37968072b0086
IMFreelancer07 <imfreelancer07@gmail.com>
2023-09-28 18:39:55 +0500

    Provider-MVVM-utils!

--- lib/mvvm/utils/utils.dart
+++ lib/mvvm/utils/utils.dart
@@ -1,11 +1,43 @@
-
-import 'package:smartfluttertoast/smartfluttertoast.dart';
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 
 class Utils {
+  static toastMessage(String message) {
+    Toast.show(message,
+        backgroundColor: Colors.black,
+        textStyle: TextStyle(color: Colors.white));
+  }
 
-  static toastMessage(String message){
+  static void flushBarErrorMessages(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        message: message,
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        forwardAnimationCurve: Curves.decelerate,
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.red,
+        reverseAnimationCurve: Curves.easeInOut,
+        positionOffset: 20,
+        icon: Icon(
+          Icons.error,
+          size: 28,
+          color: Colors.white,
+        ),
+        flushbarPosition: FlushbarPosition.TOP,
+      )..show(context),
+    );
+  }
 
-    Smartfluttertoast().showToast(message);
-    // Fluttertoast.showToast(msg: message);
+  static snakBar(String message, BuildContext context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(backgroundColor: Colors.red, content: Text(message)),
+    );
   }
 }
--- lib/mvvm/view/login_screen.dart
+++ lib/mvvm/view/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_flutter/mvvm/utils/routes/routes_name.dart';
 import 'package:test_flutter/mvvm/utils/utils.dart';
+import 'package:toast/toast.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,22 +13,21 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    ToastContext().init(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Login"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Center(
-            child: InkWell(
-                onTap: (){
-                  Utils.toastMessage("Clicked Login!");
-                },
-                child: Text("Click to go home"),
-            ),
-          )
-        ],
+      body: Center(
+        child: InkWell(
+            onTap: (){
+              Utils.toastMessage("Clicked Login!");
+              Utils.snakBar("SnackBarMsg", context);
+              Utils.flushBarErrorMessages("FlushBarMsg", context);
+            },
+            child: Text("Click to go home"),
+        ),
       ),
     );
   }
