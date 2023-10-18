ec2f99b35fecb86a207b18064cbde6facf08c13f
Imfreelancer07 <imfreelancer07@gmail.com>
2023-10-18 18:56:57 +0500

    Flutter-SocialMedia-appTheme!

--- lib/firebaseSocialMediaApp/mainSocialMedia.dart
+++ lib/firebaseSocialMediaApp/mainSocialMedia.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_res/socialMedia_color.dart';
+import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_res/socialMedia_fonts.dart';
 import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_utils/routes/socialMedia_route_name.dart';
 import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_utils/routes/socialMedia_routes.dart';
 import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_view/splash/socialMedia_splash_screen.dart';
@@ -17,7 +19,26 @@ class _mainSocialMediaState extends State<mainSocialMedia> {
       title: 'Flutter SocialMedia',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: AppColors.primaryMaterialColor,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          color: AppColors.whiteColor,
+          centerTitle: true,
+          titleTextStyle: TextStyle(fontSize: 22, fontFamily: AppFonts.sfProDisplayMedium, color: AppColors.primaryTextTextColor)
+        ),
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 40, fontFamily: AppFonts.sfProDisplayMedium, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w500, height: 1.6),
+          headline2: TextStyle(fontSize: 32, fontFamily: AppFonts.sfProDisplayMedium, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w500, height: 1.6),
+          headline3: TextStyle(fontSize: 28, fontFamily: AppFonts.sfProDisplayMedium, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w500, height: 1.9),
+          headline4: TextStyle(fontSize: 24, fontFamily: AppFonts.sfProDisplayMedium, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w500, height: 1.6),
+          headline5: TextStyle(fontSize: 20, fontFamily: AppFonts.sfProDisplayMedium, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w500, height: 1.6),
+          headline6: TextStyle(fontSize: 17, fontFamily: AppFonts.sfProDisplayBold, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w700, height: 1.6),
+
+          bodyText1: TextStyle(fontSize: 17, fontFamily: AppFonts.sfProDisplayBold, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w700, height: 1.6),
+          bodyText2: TextStyle(fontSize: 14, fontFamily: AppFonts.sfProDisplayRegular, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w400, height: 1.6),
+
+          caption: TextStyle(fontSize: 12, fontFamily: AppFonts.sfProDisplayBold, color: AppColors.primaryTextTextColor, fontWeight: FontWeight.w400, height: 2.26),
+        ),
       ),
       home: const socialMedia_SplashScreen(),
       initialRoute: socialMedia_RouteName.splashScreen,
