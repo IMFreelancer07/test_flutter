d8199a22089d3dadbd9bc09280a9a5685ca9f2bb
Imfreelancer07 <imfreelancer07@gmail.com>
2023-10-19 18:40:57 +0500

    SocialMedia-component-InputField!

---
+++ lib/firebaseSocialMediaApp/socialMedia_res/components/socialMedia_input_text_field.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_res/socialMedia_color.dart';
+
+class socialMedia_inputFormField extends StatelessWidget {
+  socialMedia_inputFormField(
+      {super.key,
+      required this.myController,
+      required this.focusNode,
+      required this.onFieldSubmittedValue,
+      required this.onValidator,
+      required this.keyBoardType,
+      required this.hint,
+      required this.obscureText,
+      this.enable = true,
+      this.autoFocus = false});
+
+  final TextEditingController myController;
+  final FocusNode focusNode;
+  final FormFieldSetter onFieldSubmittedValue;
+  final FormFieldValidator onValidator;
+
+  final TextInputType keyBoardType;
+  final String hint;
+  final bool obscureText;
+  final bool enable, autoFocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: TextFormField(
+        controller: myController,
+        focusNode: focusNode,
+        onFieldSubmitted: onFieldSubmittedValue,
+        validator: onValidator,
+        keyboardType: keyBoardType,
+        obscureText: obscureText,
+        autofocus: autoFocus,
+        style: Theme.of(context).textTheme.bodyText2!.copyWith(height: 0, fontSize: 19),
+        decoration: InputDecoration(
+          hintText: hint,
+          enabled: enable,
+          contentPadding: EdgeInsets.all(15),
+          hintStyle: Theme.of(context)
+              .textTheme
+              .bodyText2!
+              .copyWith(height: 0, color: AppColors.primaryTextTextColor.withOpacity(0.8)),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(color: AppColors.textFieldDefaultFocus),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: AppColors.secondaryColor),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          errorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: AppColors.alertColor),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: AppColors.textFieldDefaultBorderColor),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/firebaseSocialMediaApp/socialMedia_view/login/socialMedia_login_screen.dart
+++ lib/firebaseSocialMediaApp/socialMedia_view/login/socialMedia_login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_res/components/socialMedia_input_text_field.dart';
 import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_res/components/socialMedia_round_button.dart';
 
 class socialMedia_loginScreen extends StatefulWidget {
@@ -9,18 +10,39 @@ class socialMedia_loginScreen extends StatefulWidget {
 }
 
 class _socialMedia_loginScreenState extends State<socialMedia_loginScreen> {
+
+  final emailController = TextEditingController();
+  final emailFocusNode = FocusNode();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          socialMedia_RoundButton(
-              title: "Login",
-              onPress: (){},
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            socialMedia_inputFormField(
+                myController: emailController,
+                focusNode: emailFocusNode,
+                onFieldSubmittedValue: (value){},
+                onValidator: (value){
+                  return value.isEmpty ? "Enter email" : null;
+                },
+                keyBoardType: TextInputType.emailAddress,
+                hint: "Email",
+                obscureText: false
+            ),
+            SizedBox(height: 40,),
+            socialMedia_RoundButton(
+                title: "Login",
+                onPress: (){},
+            )
+          ],
+        ),
       ),
     );
   }
