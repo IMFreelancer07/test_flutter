b38537cca525a8a5c882fd800c50f3089d2f9871
Imfreelancer07 <imfreelancer07@gmail.com>
2023-10-27 22:27:48 +0500

    SocialMedia-BottomNavBar!

--- lib/firebaseSocialMediaApp/socialMedia_view/dashboard/socialMedia_dashboard_screen.dart
+++ lib/firebaseSocialMediaApp/socialMedia_view/dashboard/socialMedia_dashboard_screen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_ViewModel/services/socialMedia_sessionManager.dart';
+import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_res/socialMedia_color.dart';
 import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_utils/routes/socialMedia_route_name.dart';
 import 'package:test_flutter/firebaseSocialMediaApp/socialMedia_utils/socialMedia_utils.dart';
 import 'package:toast/toast.dart';
@@ -13,40 +15,81 @@ class socialMedia_DashboardScreen extends StatefulWidget {
 }
 
 class _socialMedia_DashboardScreenState extends State<socialMedia_DashboardScreen> {
+
+  final controller = PersistentTabController(initialIndex: 0);
+
+  List<Widget> _buildScreen(){
+    return [
+      Text("Home"),
+      Text("Chat"),
+      Text("Add"),
+      Text("Message"),
+      Text("Profile"),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarItem() {
+    return [
+      PersistentBottomNavBarItem(
+          icon: Icon(
+              Icons.home,
+          ),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(
+          Icons.home_outlined,
+          color: Colors.grey.shade50,
+        )
+      ),
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.chat),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(
+            Icons.chat_outlined,
+            color: Colors.grey.shade50,
+          )
+      ),
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.add),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(
+            Icons.add_outlined,
+            color: Colors.grey.shade50,
+          )
+      ),
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.message),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(
+            Icons.message_outlined,
+            color: Colors.grey.shade50,
+          )
+      ),
+      PersistentBottomNavBarItem(
+          icon: Icon(Icons.person),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(
+            Icons.person_outline,
+            color: Colors.grey.shade50,
+          )
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     final _auth = FirebaseAuth.instance;
     ToastContext().init(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Wellcome to Dashboard"),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-              onPressed: () {
-                _auth.signOut().then((value) {
-                  socialMedia_sessionManager().userId = "";
-                  socialMedia_utils.toastMessage_socialMedia("Logged Out successfully", true);
-                  Navigator.pushNamed(
-                      context,
-                      socialMedia_RouteName.loginView);
-                }).onError((error, stackTrace) {
-                  socialMedia_utils.toastMessage_socialMedia(
-                      "Error Occured \n" + error.toString(), false);
-                });
-              },
-              icon: Icon(Icons.logout)),
-          SizedBox(
-            width: 10,
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
 
-        ],
-      ),
+    return PersistentTabView(
+        context,
+        screens: _buildScreen(),
+        items: _navBarItem(),
+        controller: controller,
+        backgroundColor: AppColors.otpHintColor,
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(1),
+        ),
+        navBarStyle: NavBarStyle.style15,
     );
   }
 }
